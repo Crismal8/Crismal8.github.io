01638a2bb9daa62211b4dd6f391b9632642b5ca8
Crismal8 <crismalpena@gmail.com>
2025-07-08 16:43:05 -0400

    Update jsdemo.js

--- jsdemo.js
+++ jsdemo.js
@@ -2,3 +2,11 @@ console.log("This output is from the external Js file");
 console.log("Name: crismal");
 console.log("school:julius L.chambers");
 console.log("Grade:senior");
+
+var name ="crismal";
+let school=" julius L. chambers";
+var=grade = "senior";
+
+document.getElementById("name").innerHTML = "Name:<b>"+name+",</b>";
+document.getElementById("grade").innerHTML = "Name:<b>"+grade+",</b>";
+document.getElementById("school").innerHTML = "Name:<b>"+school+",</b>";
