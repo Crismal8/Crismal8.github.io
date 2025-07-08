fd5fecf6af2dcddcafed966695dfada1f9e90cf6
Crismal8 <crismalpena@gmail.com>
2025-07-08 16:14:54 -0400

    Update jsdemo.js

--- jsdemo.js
+++ jsdemo.js
@@ -1,4 +1,4 @@
 console.log("This output is from the external Js file");
 console.log("Name: crismal");
 console.log("school:julius L.chambers");
-comsole.log("Grade:senior");
+console.log("Grade:senior");
