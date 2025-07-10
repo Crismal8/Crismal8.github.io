066e0ae7a3eef166e6375fedd4d87651f8d155d3
Crismal8 <crismalpena@gmail.com>
2025-07-10 16:54:10 -0400

    Update jsreview.js

--- jsreview.js
+++ jsreview.js
@@ -2,3 +2,18 @@ var course = "technology";
 let myName = "Crismal";
 
 console.log(`This page was created in the STEM PREP ${course} by ${myName}`);
+
+alert(`This page was created in the STEM PREP ${course} by ${myName}`);
+
+function output()
+{
+let name = document.getElementById("name").Value;
+  var school = document.getElementyById("school").value;
+  let grade = document.getElementById("grade").value;
+
+let output ="Name:<b>"+name+"</b><br>";
+output+="Grade:<b>" +grade + "</b><br>";
+output+= "school: <b>" + school + "</b><br>";  
+
+document.getElementById("output").innerHTML = output;  
+}
