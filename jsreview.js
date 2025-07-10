184dfbc69479d71c758674d87db1a16f515177a7
Crismal8 <crismalpena@gmail.com>
2025-07-10 17:11:54 -0400

    Update jsreview.js

--- jsreview.js
+++ jsreview.js
@@ -7,12 +7,12 @@ alert(`This page was created in the STEM PREP ${course} by ${myName}`);
 
 function output()
 {
-let name = document.getElementById("name").Value;
+let name = document.getElementById("name").value;
   var school = document.getElementById("school").value;
   let grade = document.getElementById("grade").value;
 
-let output ="Name:<b>"+name+"</b><br>";
-output+="Grade:<b>" +grade + "</b><br>";
+let output ="Name: <b>"+name+"</b><br>";
+output+="Grade: <b>" +grade + "</b><br>";
 output+= "school: <b>" + school + "</b><br>";  
 
 document.getElementById("output").innerHTML = output;  
