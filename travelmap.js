edb3182bf0b1b1aeb446bdabfc3c4c03e26cb0c9
Crismal8 <crismalpena@gmail.com>
2025-07-17 16:52:28 -0400

    Update travelmap.js

--- travelmap.js
+++ travelmap.js
@@ -14,6 +14,6 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 let circlemarker =L.circle(coordinates, {
   color: "black",
   fillcolor: "yellow",
-  fillOpacity:0.25,
+  fillOpacity: 0.25,
   radius: 50
 }).bindpopup(`<iframe width="280" height="160" src="https://www.youtube.com/embed/xiN4ZkEc1O0?si=dbVgWhL6aKYurppt" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`).addTo(map);
